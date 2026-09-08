91b3b20252bc27dd3fcb818f91e5e0c71dc92a0c
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-08 15:39:02 +0700

    feat: implement generic repository base

--- src/ClinicManagementSystem.Infrastructure/Repositories/IRepositoryBase.cs
+++ src/ClinicManagementSystem.Infrastructure/Repositories/IRepositoryBase.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using ClinicManagementSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 public interface IRepositoryBase<T> where T : class
@@ -27,3 +29,75 @@ public interface IRepositoryBase<T> where T : class
 
     IQueryable<T> WhereSql(Expression<Func<T, bool>> predicate);
 }
+
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly ClinicManagementDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public RepositoryBase(ClinicManagementDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            return await Task.FromResult(_dbSet.AsQueryable());
+        }
+
+        public async Task<T?> GetByIdAsync(dynamic id)
+        {
+            return await _dbSet.FindAsync(new object?[] { id });
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(dynamic id)
+        {
+            var entity = await GetByIdAsync(id);
+
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public async Task<T?> SingleOrDefault(
+            Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.FirstOrDefaultAsync(predicate);
+        }
+
+        public Task<IQueryable<T>> Where(
+            Expression<Func<T, bool>> predicate)
+        {
+            return Task.FromResult(_dbSet.Where(predicate));
+        }
+
+        public async Task AddListItemsAsync(List<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        public Task UpdateListItemsAsync(List<T> entities)
+        {
+            _dbSet.UpdateRange(entities);
+            return Task.CompletedTask;
+        }
+
+        public IQueryable<T> WhereSql(
+            Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
