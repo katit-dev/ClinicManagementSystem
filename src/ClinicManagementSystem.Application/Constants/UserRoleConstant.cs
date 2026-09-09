7f36aaea23aa25c34cc2e297ad367c4aa187dbb2
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-09 12:50:19 +0700

    feat: add user role constants

---
+++ src/ClinicManagementSystem.Application/Constants/UserRoleConstant.cs
@@ -0,0 +1,9 @@
+namespace ClinicManagementSystem.Application.Constants;
+
+public static class UserRoleConstant
+{
+    public const string Admin = "Admin";
+    public const string Receptionist = "Receptionist";
+    public const string Doctor = "Doctor";
+    public const string Patient = "Patient";
+}
