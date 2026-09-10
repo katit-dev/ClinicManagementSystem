be29e4b1f96ab514aceaedca8b24849880d3ca29
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-10 15:53:51 +0700

    data for register-test api

--- database/seed_for_test.sql
+++ database/seed_for_test.sql
@@ -28,3 +28,41 @@ WHERE patient_code = 'BNTEST000000000001';
 
 SELECT *
 FROM auth.roles;
+
+--kiem tra db sau khi test register api success
+SELECT *
+FROM auth.users
+WHERE phone = '0901111111';
+
+SELECT *
+FROM scheduling.patients
+WHERE phone = '0901111111';
+
+SELECT ur.*
+FROM auth.user_roles ur
+JOIN auth.users u ON u.id = ur.user_id
+WHERE u.phone = '0901111111';
+
+
+-- Có Patient cũ nhưng chưa có account
+INSERT INTO scheduling.patients
+(
+    user_id,
+    full_name,
+    date_of_birth,
+    phone,
+    patient_code
+)
+VALUES
+(
+    NULL,
+    N'Trần Thị Vãng Lai',
+    '1995-08-20',
+    '0902222222',
+    'BNTESTVANGLAI00001'
+);
+
+SELECT id, user_id, full_name, phone, patient_code
+FROM scheduling.patients
+WHERE phone = '0902222222';
+
