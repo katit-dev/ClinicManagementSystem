6f0e57b6133602d438086370b7f16bd8cf61398f
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-17 23:49:24 +0700

    feat(application): add available slot response messages

--- src/ClinicManagementSystem.Application/DTOs/Doctor/DoctorResponseMessageDTO.cs
+++ src/ClinicManagementSystem.Application/DTOs/Doctor/DoctorResponseMessageDTO.cs
@@ -10,4 +10,17 @@ public static class DoctorResponseMessageDTO
 
     public const string InvalidSpecialty =
         "Chuyên khoa không hợp lệ.";
+
+    // cho available slot
+    public const string GetAvailableSlotsSuccess =
+    "Lấy danh sách khung giờ khám thành công.";
+
+    public const string GetAvailableSlotsFailed =
+        "Không thể lấy danh sách khung giờ khám.";
+
+    public const string DoctorNotFound =
+        "Không tìm thấy bác sĩ.";
+
+    public const string InvalidAppointmentDate =
+        "Ngày khám không hợp lệ.";
 }
