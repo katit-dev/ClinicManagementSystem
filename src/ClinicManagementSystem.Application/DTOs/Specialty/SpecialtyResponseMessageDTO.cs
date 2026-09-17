578d33bc024b832f159d80f2185f66fa89c327ca
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-17 15:00:03 +0700

    feat(application): add specialty response messages

---
+++ src/ClinicManagementSystem.Application/DTOs/Specialty/SpecialtyResponseMessageDTO.cs
@@ -0,0 +1,10 @@
+namespace ClinicManagementSystem.Application.DTOs.Specialty;
+
+public static class SpecialtyResponseMessageDTO
+{
+    public const string GetListSuccess =
+        "Lấy danh sách chuyên khoa thành công.";
+
+    public const string GetListFailed =
+        "Không thể lấy danh sách chuyên khoa.";
+}
