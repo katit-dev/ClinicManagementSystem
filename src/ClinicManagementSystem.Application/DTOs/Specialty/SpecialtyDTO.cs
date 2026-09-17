8d00843aca273c9ef38774ebee824daa4f280b4e
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-17 14:05:03 +0700

    feat(application): add specialty DTO

---
+++ src/ClinicManagementSystem.Application/DTOs/Specialty/SpecialtyDTO.cs
@@ -0,0 +1,9 @@
+namespace ClinicManagementSystem.Application.DTOs.Specialty;
+
+public class SpecialtyDTO
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+        = string.Empty;
+}
