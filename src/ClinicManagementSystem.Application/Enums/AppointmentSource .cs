f8ed60c17a6a4f875ef293bfcf89a952c39f134b
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-19 10:56:08 +0700

    add appointment source enum

---
+++ src/ClinicManagementSystem.Application/Enums/AppointmentSource .cs
@@ -0,0 +1,15 @@
+namespace ClinicManagementSystem.Application.Enums;
+
+
+// =====================================================
+// APPOINTMENT SOURCE
+// =====================================================
+
+public enum AppointmentSource : byte
+{
+    Online = 0,
+
+    WalkIn = 1,
+
+    Phone = 2
+}
