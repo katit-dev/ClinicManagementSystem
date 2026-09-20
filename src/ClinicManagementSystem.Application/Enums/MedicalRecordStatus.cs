051832d9f707acc6ef3a3192943fc69aff2d4ba9
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-20 13:22:12 +0700

    add medical record status enum

---
+++ src/ClinicManagementSystem.Application/Enums/MedicalRecordStatus.cs
@@ -0,0 +1,13 @@
+namespace ClinicManagementSystem.Application.Enums;
+
+
+// =====================================================
+// MEDICAL RECORD STATUS
+// =====================================================
+
+public enum MedicalRecordStatus : byte
+{
+    Draft = 0,
+
+    Finalized = 1
+}
