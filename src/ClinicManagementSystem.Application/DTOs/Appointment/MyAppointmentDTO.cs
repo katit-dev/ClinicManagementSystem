a03e9c004505b94426e57c115f27714713ac466c
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-22 13:08:30 +0700

    add DoctorId in MyAppointmentDTO

--- src/ClinicManagementSystem.Application/DTOs/Appointment/MyAppointmentDTO.cs
+++ src/ClinicManagementSystem.Application/DTOs/Appointment/MyAppointmentDTO.cs
@@ -1,57 +1,22 @@
 namespace ClinicManagementSystem.Application.DTOs.Appointment;
 
-
-// =====================================================
-// MY APPOINTMENT DTO
-// =====================================================
-
 public class MyAppointmentDTO
 {
-    // =================================================
-    // APPOINTMENT
-    // =================================================
-
     public int Id { get; set; }
 
-    public string AppointmentCode { get; set; }
-        = string.Empty;
+    public string AppointmentCode { get; set; } = string.Empty;
 
+    public int DoctorId { get; set; }
 
-    // =================================================
-    // DOCTOR
-    // =================================================
+    public string DoctorName { get; set; } = string.Empty;
 
-    public string DoctorName { get; set; }
-        = string.Empty;
-
-
-    // =================================================
-    // SPECIALTY
-    // =================================================
-
-    public string SpecialtyName { get; set; }
-        = string.Empty;
-
-
-    // =================================================
-    // APPOINTMENT TIME
-    // =================================================
+    public string SpecialtyName { get; set; } = string.Empty;
 
     public DateTime StartTime { get; set; }
 
     public DateTime EndTime { get; set; }
 
-
-    // =================================================
-    // STATUS
-    // =================================================
-
     public byte Status { get; set; }
-
-
-    // =================================================
-    // QUEUE
-    // =================================================
 
     public int? QueueNumber { get; set; }
 }
