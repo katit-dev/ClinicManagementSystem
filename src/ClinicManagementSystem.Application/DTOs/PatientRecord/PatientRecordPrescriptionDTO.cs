6a59fd153d0370459d02be013b0d80c4d29b84bc
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-09-23 10:03:34 +0700

    feat(application): add PatientRecordPrescriptionDTO.cs

---
+++ src/ClinicManagementSystem.Application/DTOs/PatientRecord/PatientRecordPrescriptionDTO.cs
@@ -0,0 +1,21 @@
+namespace ClinicManagementSystem.Application.DTOs.PatientRecord;
+
+
+// =====================================================
+// PATIENT RECORD PRESCRIPTION DTO
+// =====================================================
+
+public class PatientRecordPrescriptionDTO
+{
+    public int Id { get; set; }
+
+    public string? Note { get; set; }
+
+    public byte Status { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? DispensedAt { get; set; }
+
+    public List<PatientRecordPrescriptionItemDTO> Items { get; set; } = new();
+}
